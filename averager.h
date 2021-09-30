bfaa72dcb413c52c4c7acc7648e4dc49959fcc95
Андрей Заболотский <46747710+AndreiZab@users.noreply.github.com>
2021-09-30 20:00:05 +0300

    simple implementation with big vector

--- averager.h
+++ averager.h
@@ -1,12 +1,15 @@
+#include <vector>
+
+
 #define ERROR_TOO_LARGE_WINDOW -1
 #define ERROR_ZERO_WINDOW      -2
-#include <vector>
-#include <stdio.h>
+#define ERROR_NO_DATA          -3
 
+#include <stdio.h>
 
 typedef struct {
     uint32_t value;
-    time_t time_stamp;
+    time_t time_stamp; // 32-bit system 'Year 2038 problem'
 }
 rx_value_t;
 
@@ -23,13 +26,16 @@ public:
 
     /* Возвращается среднее значение скорости */
     float GetAverageSpeed() {
-        uint32_t sum = 0;
+        float n = 1;
         float avg_spd = -1;
         uint32_t avg_time_s = p_avg_time_s;
         time_t curr_time = time(nullptr);
 
         printf("s:%lu, a:%d\n", value_vector.size(), avg_time_s);
 
+        if (value_vector.empty()) {
+            return ERROR_NO_DATA;
+        }
         if (!value_vector.empty() && value_vector[0].time_stamp > (curr_time - avg_time_s)) {
             return ERROR_TOO_LARGE_WINDOW;
         }
@@ -40,11 +46,12 @@ public:
 
         auto i = value_vector.size() - 1;
         while (value_vector[i].time_stamp > (curr_time - avg_time_s)) {
-            sum += value_vector[i].value;
+            avg_spd = avg_spd + ((float)value_vector[i].value - avg_spd) / n;
+            n++;
             i--;
         }
 
-        avg_spd = ((float)sum)/((float)avg_time_s);
+//        avg_spd = ((float)sum)/((float)avg_time_s);
         return avg_spd;
     };
 
@@ -52,16 +59,18 @@ public:
     void AddValue(uint32_t value) {
         rx_value_t rx_value;
 
+        // TODO syscall all time - bad solution
         rx_value.time_stamp = time(nullptr);
         rx_value.value = value;
 
         // all value in one sec fits into 1 element
         if (!value_vector.empty() && value_vector.back().time_stamp == rx_value.time_stamp ) {
             // overflow protect
-            if ((value_vector.back().value + value) > value) {
+            if ((value_vector.back().value + value) >= value) {
                 value_vector.back().value += value;
             }
             else {
+                printf("1: %u,2: %u\n", value_vector.back().value, value);
                 value_vector.back().value = UINT32_MAX;
             }
         }
