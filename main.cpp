8fc1f407201490f18bdb566b6172af290b76b525
Андрей Заболотский <46747710+AndreiZab@users.noreply.github.com>
2021-10-02 22:35:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
-#include <stdint.h>
+#include <cstdint>
+#include <unistd.h>
+
 #include "cmake-build-debug/averager.h"
+
+
+#define TARGET_WINDOW_S 5
+
 
 
 int main() {
 
-    uint32_t rx_byte = 0;
+    uint64_t elem_summ = 0;
     Averager network(0);
-    network.SetAveragingTime(10);
+    network.SetAveragingTime(TARGET_WINDOW_S);
 
-    while(1) {
-        std::cin >> rx_byte;
-        network.AddValue(rx_byte);
-        printf("avg_speed:%f\n", network.GetAverageSpeed());
+    for (int i = 0; i < 60; i++){
+        uint32_t x = (uint32_t) rand()% 5000;
+
+
+        network.AddValue(x);
+
+        usleep(100000);
+        if ((60 - i) <= (TARGET_WINDOW_S*10)) {
+            elem_summ += x;
+        }
     }
+    printf("avg_speed:%f (%f)\n", network.GetAverageSpeed(), (float)elem_summ/(float)TARGET_WINDOW_S);
 
     return 0;
 }
