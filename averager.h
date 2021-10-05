e6741bb6d56d21ce6a28c5d0be7c85b980e88634
Андрей Заболотский <46747710+AndreiZab@users.noreply.github.com>
2021-10-05 14:07:43 +0300

    convert to float

--- averager.h
+++ averager.h
@@ -1,6 +1,5 @@
 #include <vector>
 
-
 #define ERROR_TOO_LONG_WINDOW -1
 #define ERROR_ZERO_WINDOW     -2
 #define NO_DATA                0
@@ -10,7 +9,7 @@
 #define VEC_TARGET_SIZE 100
 
 typedef struct {
-    uint32_t avg; // средняя скорость
+    float avg; // средняя скорость
     time_t dur_s; // продолжительность временного отрезка
     time_t start_s; // время начало отрезка (Unix time)
     // TODO 32-bit architecture: 'Year 2038 problem'
@@ -52,6 +51,7 @@ public:
 
     /* Добавлем информацию о получении value байтов */
     void AddValue(uint32_t value) {
+        float rx_val = (float)value;
         avg_chunk_t new_elem;
         avg_chunk_t *last = nullptr;
         if (!small.empty()) {
@@ -62,20 +62,20 @@ public:
 
         // если в эту секунду уже принимали данные, добавляем их к имеющимся
         if (!small.empty() && (curr_time <= last->dur_s + last->start_s - 1)) {
-            last->avg = (last->avg * last->dur_s + value) / last->dur_s;
+            last->avg = (last->avg * (float)last->dur_s + rx_val) / (float)last->dur_s;
         }
         // проверка на то, можно ли добавить элемент в уже созданную ячейку
-        // можно, если он не сильно отклоняет avg и присутствует последовательность во времени приема
+        // можно если он не сильно отклоняет avg и присутствие последовательность во времени приема
         else if ((!small.empty()) &&
-                 (num_div_percent((float)last->avg, (float)value, PERCENT_DEVIATION)) &&
+                 (num_div_percent(last->avg, rx_val, PERCENT_DEVIATION)) &&
                  (curr_time <= (last->start_s + last->dur_s + 1))) {
             // TODO need overflow protect
-            last->avg = (last->avg * last->dur_s + value) / (last->dur_s + 1);
+            last->avg = (last->avg * (float)last->dur_s + rx_val) / ((float)last->dur_s + 1.0f);
             last->dur_s++;
         }
         // создаем новый элемент
         else {
-            new_elem.avg = value;
+            new_elem.avg = rx_val;
             new_elem.dur_s = 1;
             new_elem.start_s = curr_time;
             small.push_back(new_elem);
@@ -89,7 +89,7 @@ public:
         if (mid.size() > VEC_TARGET_SIZE) {
             average_and_transfer_data(mid, large);
         }
-        // если данных накопилось предельно много, сохраняем их в переменную с максимальным усреднением
+        // если данных накопилось предельного много, сохраняем их в переменную с максимальным усреднением
         if (large.size() > VEC_TARGET_SIZE) {
             avg_chunk_t src_sum;
             uint32_t full_duration =
@@ -97,15 +97,15 @@ public:
             memset(&src_sum, 0, sizeof(src_sum));
 
             for (auto it = begin (large); it != end (large); it++) {
-                src_sum.avg += it->avg * it->dur_s / full_duration;
+                src_sum.avg += it->avg * (float)it->dur_s / (float)full_duration;
             }
             src_sum.start_s = large.front().start_s;
             src_sum.dur_s = full_duration;
 
             // TODO need overflow protect
-            ultimate.avg = ((src_sum.dur_s * src_sum.avg)
-                            + (ultimate.dur_s * ultimate.avg))
-                           / (src_sum.dur_s + ultimate.dur_s);
+            ultimate.avg = (((float)src_sum.dur_s * src_sum.avg)
+                            + ((float)ultimate.dur_s * ultimate.avg))
+                           / (float)(src_sum.dur_s + ultimate.dur_s);
             ultimate.dur_s += src_sum.dur_s;
 
             large.clear();
@@ -113,17 +113,17 @@ public:
     };
 
 private:
-    // изначально получаемые данные хранятся в векторе 'small' с посекундным усреднением
+    // изначально получаемые данные хранятся в векторе 'small' с 1-секундным усреднением
     // когда он наполняется, находится средняя скорость и перекладывается в одну ячейку 'mid' вектора
-    // в конечном итоге при продолжительном измерении максимально усредненные данные попадут в 'ultimate' переменную
+    // в конечном итоге, при продолжительном измерении, максимально усредненные данные попадут в 'ultimate' переменную
     std::vector<avg_chunk_t> small;
     std::vector<avg_chunk_t> mid;
     std::vector<avg_chunk_t> large;
     avg_chunk_t ultimate;
 
-    uint32_t p_avg_time_s; // окно, по которому будет происходить усреднение
+    uint32_t p_avg_time_s; // окно по которому будет происходить усреднение
 
-    // если отклонение dst от src превышает div%, то возвращает false
+    // если отклонение dst от src превышает div% то возвращает false
     bool num_div_percent(const float src, const float dst, const float div) {
         bool ret;
         if (((src > dst) && ((src * (1.0f - div / 100)) < dst)) ||
@@ -136,13 +136,13 @@ private:
         return ret;
     };
 
-    // проверяем, не выходит ли запрашиваемое пользователем окно за рамки измеренного программой
+    // проверяем не выходит ли запрашиваемое пользователем окно за рамки измеренного программой
     bool window_too_long(const time_t curr_time, const uint32_t avg_time_s) {
         bool ret;
-        if ((ultimate.avg != 0 && ultimate.start_s <= (curr_time - avg_time_s)) ||
-            (!large.empty() && large.front().start_s <= (curr_time - avg_time_s)) ||
-            (!mid.empty() && mid.front().start_s <= (curr_time - avg_time_s)) ||
-            (!small.empty() && small.front().start_s <= (curr_time - avg_time_s))) {
+        if ((ultimate.avg >= 0 && ultimate.start_s      <= (curr_time - avg_time_s)) ||
+            (!large.empty()    && large.front().start_s <= (curr_time - avg_time_s)) ||
+            (!mid.empty()      && mid.front().start_s   <= (curr_time - avg_time_s)) ||
+            (!small.empty()    && small.front().start_s <= (curr_time - avg_time_s))) {
             ret = false;
         } else {
            ret = true;
@@ -158,17 +158,17 @@ private:
 
         if (!vec.empty()) {
             auto i = vec.size() - 1;
-            // обходим вектор с конца, пока время в ячейках соответствует запрашиваемому окну
+            // обходим вектор с конца пока время в ячейках соответствует запрашиваемому окну
             while ((i < vec.size()) &&
                    ((vec[i].start_s + vec[i].dur_s) > (cur_time - avg_time_s))) {
 
                 dur = vec[i].dur_s;
-                // если необходима только часть данных из ячейки
+                // если необходима только часть данных их ячейки
                 if ((dur + (*sum_dur)) > avg_time_s) {
                     dur = avg_time_s - (*sum_dur);
                 }
 
-                (*avg_spd) += ((float) vec[i].avg / (float) avg_time_s * (float) dur);
+                (*avg_spd) += (vec[i].avg / (float)avg_time_s * (float)dur);
                 (*sum_dur) += dur;
                 i--;
             }
@@ -185,7 +185,7 @@ private:
         sum_elem(mid, cur_time, avg_time_s, &avg_spd, &sum_dur);
         sum_elem(large, cur_time, avg_time_s, &avg_spd, &sum_dur);
         if ((ultimate.start_s + ultimate.dur_s) > (cur_time - avg_time_s)) {
-            avg_spd += ((float) ultimate.avg / (float) avg_time_s * (float) (avg_time_s - sum_dur));
+            avg_spd += (ultimate.avg / (float)avg_time_s * (float)(avg_time_s - sum_dur));
         }
 
         return avg_spd;
@@ -206,20 +206,20 @@ private:
         full_src_duration = src.back().dur_s + src.back().start_s - src.front().start_s ;
 
         for (auto it = begin (src); it != end (src); it++) {
-            src_sum.avg += it->avg * it->dur_s / full_src_duration;
+            src_sum.avg += it->avg * (float)it->dur_s / (float)full_src_duration;
         }
         src_sum.start_s = src.front().start_s;
         src_sum.dur_s = full_src_duration;
 
         // при возможности добавляем данные в уже существующую ячейку
-        // (если нет большого отклонение в avg и присутствует последовательность во времени приема)
+        // (если нет большого отклонение в avg и присутствие последовательность во времени приема)
         if ((!dst.empty()) &&
-            (num_div_percent((float)dst.back().avg, (float)src_sum.avg, PERCENT_DEVIATION)) &&
+            (num_div_percent(dst.back().avg, src_sum.avg, PERCENT_DEVIATION)) &&
             (src_sum.start_s <= (dst.back().start_s + dst.back().dur_s + 1))) {
             // TODO need overflow protect
-            dst.back().avg = ((src_sum.dur_s * src_sum.avg)
-                            + (dst.back().dur_s * dst.back().avg))
-                            / (src_sum.dur_s + dst.back().dur_s);
+            dst.back().avg = (((float)src_sum.dur_s * src_sum.avg)
+                            + ((float)dst.back().dur_s * dst.back().avg))
+                            /  (float)(src_sum.dur_s + dst.back().dur_s);
             dst.back().dur_s += src_sum.dur_s;
         }
         // добавляем новый элемент в вектор с большим усреднением
